3e3e2bcb87e776933fc62860dbf30f33f758cf14
Warren <rabbitsinwarrens@gmail.com>
2024-01-26 11:37:43 +1100

    test(task0): fail has shebang line

--- python-hello_world/tests/test_hello_world_project.py
+++ python-hello_world/tests/test_hello_world_project.py
@@ -3,3 +3,11 @@ import pytest
 def test_that_file_exists():
     with open("2-print.py"):
         assert True
+
+def test_that_file_has_correct_shebang():
+    expected = '#!/usr/bin/env python3'
+
+    with open("2-print.py") as file:
+        first_line = file.readline()
+        assert first_line == expected
+

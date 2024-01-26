bd280da537ac22a60c8b7e94a8d405a807df59d7
Warren <rabbitsinwarrens@gmail.com>
2024-01-27 10:38:05 +1100

    chore(test): rename tests

--- python-hello_world/tests/test_hello_world_project.py
+++ python-hello_world/tests/test_hello_world_project.py
@@ -1,25 +1,23 @@
-import pytest
 import os
+import ast
+import subprocess
 
 FILE = "2-print.py"
 
-def test_that_file_exists():
+def test_that_task0_file_exists():
     with open(FILE):
         assert True
 
-
-def test_that_file_has_correct_shebang():
+def test_that_task0_file_has_correct_shebang():
     expected = '#!/usr/bin/env python3\n'
     with open(FILE) as file:
         first_line = file.readline()
         assert first_line == expected
 
-
-def test_that_file_is_executable():
+def test_that_task0_file_is_executable():
     assert os.access(FILE, os.X_OK)
 
-   
-def test_that_script_uses_print_function():
+def test_that_source_of_task0_file_contains_print():
     expected = 'print('
     with open(FILE) as file:
         for line in file:
@@ -27,6 +25,7 @@ def test_that_script_uses_print_function():
                 assert True
                 return
     assert False
+
 def test_that_ast_of_task0_file_contains_print():
     with open(FILE) as file:
         tree = ast.parse(file.read())
@@ -37,3 +36,10 @@ def test_that_ast_of_task0_file_contains_print():
                 assert True
                 return
     assert False
+    
+def test_that_task0_file_outputs_correct_string():
+    expected = "Programming is like building a multilingual puzzle\n"
+    result = subprocess.run([f"./{FILE}"], capture_output=True, text=True)
+    assert result.stdout == expected
+    
+    

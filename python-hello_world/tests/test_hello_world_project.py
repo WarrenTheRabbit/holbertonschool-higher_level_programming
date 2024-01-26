6a8a634c82d2fa4739d463d72cd8be5cff073c25
Warren <rabbitsinwarrens@gmail.com>
2024-01-26 21:52:25 +1100

    refactor: use constant for filename

--- python-hello_world/tests/test_hello_world_project.py
+++ python-hello_world/tests/test_hello_world_project.py
@@ -1,13 +1,18 @@
 import pytest
+import os
+
+FILE = "2-print.py"
 
 def test_that_file_exists():
-    with open("2-print.py"):
+    with open(FILE):
         assert True
 
 def test_that_file_has_correct_shebang():
-    expected = '#!/usr/bin/env python3'
-
-    with open("2-print.py") as file:
+    expected = '#!/usr/bin/env python3\n'
+    with open(FILE) as file:
         first_line = file.readline()
         assert first_line == expected
 
+def test_that_file_is_executable():
+    assert os.access(FILE, os.X_OK)
+    

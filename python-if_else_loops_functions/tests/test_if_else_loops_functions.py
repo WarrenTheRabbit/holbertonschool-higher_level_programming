41c07934994c4eba567f74eb8b4ea9ae4c34e863
Warren <rabbitsinwarrens@gmail.com>
2024-01-28 12:42:31 +1100

    test(task0): require file existence

--- python-if_else_loops_functions/tests/test_if_else_loops_functions.py
+++ python-if_else_loops_functions/tests/test_if_else_loops_functions.py
@@ -1,4 +1,14 @@
 import pytest
 
-def test_that_demonstrates_failure():
-    pytest.fail("This test fails.")
+TASK0 = "0-positive_or_negative.py"
+
+
+@pytest.fixture(params=[TASK0])
+def task_file(request):
+    return request.param
+
+def test_that_task_file_exists(task_file):
+    with open(task_file):
+        assert True
+        return
+    pytest.fail("Task file does not exist.")

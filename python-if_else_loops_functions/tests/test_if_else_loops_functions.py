20f4def5b5e1d14c77b2a3ab24c136e511900cd6
Warren <rabbitsinwarrens@gmail.com>
2024-01-28 12:52:00 +1100

    test(task0): require Python shebang

--- python-if_else_loops_functions/tests/test_if_else_loops_functions.py
+++ python-if_else_loops_functions/tests/test_if_else_loops_functions.py
@@ -2,13 +2,24 @@ import pytest
 
 TASK0 = "0-positive_or_negative.py"
 
-
+#######################################
+# Fixtures
+#######################################
 @pytest.fixture(params=[TASK0])
 def task_file(request):
     return request.param
 
+
+########################################
+# Tests
+########################################
 def test_that_task_file_exists(task_file):
     with open(task_file):
         assert True
         return
-    pytest.fail("Task file does not exist.")
+    
+def test_that_task_file_has_python_shebang(task_file):
+    expected_shebang_line = '#!/usr/bin/python3\n'
+    with open(task_file) as opened_file:
+        first_line = opened_file.readline()
+        assert first_line == expected_shebang_line

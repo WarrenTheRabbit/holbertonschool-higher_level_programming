9d06688e7aa7dee291407c0c8fa20e2408af5583
Warren <rabbitsinwarrens@gmail.com>
2024-01-30 13:15:54 +1100

    fix(task0): use string format

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end='')
+            print("{}".format(my_list[i]), end="")
             count += 1
     except IndexError as e:
         ...
     finally:
-        print("")
+        print()
         return count

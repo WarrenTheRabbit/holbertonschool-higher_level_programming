31c4c6fb41f5721c83c9433c5a8025a08e11030f
Warren <rabbitsinwarrens@gmail.com>
2024-01-30 13:00:49 +1100

    fix(task0): correct count

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(x):
-            print("{}", my_list[i], end='')
+            print(my_list[i], end='')
+            count += 1
     except IndexError as e:
         pass
     finally:
         print()
-        return i + 1 if my_list else 0
+        return count

5117fd4451a743762a5f71006e3529e858a39fc3
Warren <rabbitsinwarrens@gmail.com>
2024-01-30 13:09:50 +1100

    chore(task0): submit for checker

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -6,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             count += 1
     except IndexError as e:
-        pass
+        ...
     finally:
-        print()
+        print("")
         return count

47f0128e0c3fcc5e8680326c8bcc0f5a137e571f
Warren <rabbitsinwarrens@gmail.com>
2024-01-31 15:47:17 +1100

    fix(task0): remove add function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 import importlib
 
-def add(a, b):
-    return a + b
-
-# This would be in your main file, e.g., '0-add.py'
 if __name__ == "__main__":
     func = importlib.import_module("0-add").add
     a = 1

f52e22b94c2ea1a8fe4b15235864e5869dfa1499
Warren <rabbitsinwarrens@gmail.com>
2024-01-31 15:16:00 +1100

    feat(task4): submit to checker

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    try:
+        for i in range(list_length):
+            try:
+                result = my_list_1[i]/ my_list_2[i]
+                new_list.append(result)
+            except TypeError:
+                new_list.append(0)
+                print("wrong type")
+            except ZeroDivisionError:
+                new_list.append(0)
+                print("divison by 0")     
+    except IndexError:
+        print("out of range")
+    finally:
+        return new_list
+        

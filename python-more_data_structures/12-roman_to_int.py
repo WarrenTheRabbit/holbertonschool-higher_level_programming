2967625f444e9501ca5b079eb87d103d02f29783
Warren <rabbitsinwarrens@gmail.com>
2024-01-31 22:00:50 +1100

    feat(task12): submit to checker

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    conversion = {
+        "X": 10,
+        "V": 5,
+        "L": 50,
+        "I": 1,
+        "C": 100,
+        "D": 500,
+    }
+
+    values = [conversion[symbol] for symbol in roman_string]
+    total = 0
+
+    for i in range(len(values)):
+        if i + 1 < len(values) and values[i] < values[i+1]:
+            total -= values[i]
+        else:
+            total += values[i]
+    return total

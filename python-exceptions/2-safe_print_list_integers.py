6336af2124294eab2e7f588b2d4dca29d46598ed
Warren <rabbitsinwarrens@gmail.com>
2024-01-31 14:29:28 +1100

    fix(task2): print after loop

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(0,x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
@@ -10,5 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
         except IndexError as e:
             raise e
-        print()
-        return count
+    print()
+    return count

acc916f893d125ecec9132f05c4004cc786986a0
Warren <rabbitsinwarrens@gmail.com>
2024-02-01 09:14:04 +1100

    doc(task0): add module docstring and expand class docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
 
+"""Task0 module for Holberton School Australia's OOP section.
+
+The module contains one class, Square.
+
+"""
+
+
 class Square:
-    """
-    A class Square that defines a square.
+    """Represents a square.
+
+        Attributes:
+            None
+
+        Methods:
+            None
+
+        Usage:
+            from square import Square
+
+            # Create a square object
+            square = Square()
     """
     pass

a1258665dbf1958833c90a429c7e320f1a2f859e
Warren <rabbitsinwarrens@gmail.com>
2024-01-31 15:46:01 +1100

    chore(task0): upload correct file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-
 import importlib
 
-func = importlib.import_module('0-add').add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, func(1, 2)))
+def add(a, b):
+    return a + b
+
+# This would be in your main file, e.g., '0-add.py'
+if __name__ == "__main__":
+    func = importlib.import_module("0-add").add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))

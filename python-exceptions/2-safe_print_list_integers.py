9e2a31f568b919f0755848322b710d3a72cac303
Warren <rabbitsinwarrens@gmail.com>
2024-01-31 15:28:31 +1100

    fix(task2): raises exception by itself

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    nb_print = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError) as e:
-            pass
-        except IndexError as e:
-            raise e
-    print("{}".format(""))
-    return count
+            nb_print += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return nb_print
+    

1792deef736feb1f48d8908d6f8e5da9710e4a43
Warren <rabbitsinwarrens@gmail.com>
2024-01-31 21:05:06 +1100

    fix(task3): correct variable names

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-def common_elements(set_1, set2):
-    return [element for element in set1 if element in set2]
+def common_elements(set_1, set_2):
+    return [element for element in set_1 if element in set_2]

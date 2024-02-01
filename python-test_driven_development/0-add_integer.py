91f3b5f802d7c2fbacfcf86052f4f2e0f10ec158
Warren <rabbitsinwarrens@gmail.com>
2024-02-01 21:54:15 +1100

    style(task0): satisfy pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,8 +9,8 @@ def add_integer(a, b):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     a = int(a)
     b = int(b)
-    
+
     return a + b

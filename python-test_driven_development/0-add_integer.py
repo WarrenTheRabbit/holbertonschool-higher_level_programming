b2e483e1504aea3b35d93d082a8d76b9d2596c79
Warren <rabbitsinwarrens@gmail.com>
2024-02-01 16:23:42 +1100

    feat(task0): update functionality and tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,4 +5,12 @@
 
 def add_integer(a, b):
     """Adds two integers together"""
-    return 3
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    a = int(a)
+    b = int(b)
+    
+    return a + b

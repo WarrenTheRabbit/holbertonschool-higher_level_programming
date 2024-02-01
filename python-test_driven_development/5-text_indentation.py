23240eb0e2452fe505fb99f809d3c52f1c4a8602
Warren <rabbitsinwarrens@gmail.com>
2024-02-01 22:32:51 +1100

    chore(task4): add files

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (str): text to be printed
+    """
+    did_newline = False
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if did_newline:
+            did_newline = False
+            continue
+        if text[i] in ".?:":
+            did_newline = True
+            print(text[i])
+            print()
+        else:
+            print(text[i], end="")
+    print()

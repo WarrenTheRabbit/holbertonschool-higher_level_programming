15a59d9e94a279b7bc3224c497403ad9186db798
Warren <rabbitsinwarrens@gmail.com>
2024-02-01 22:18:21 +1100

    feat(task3): submit to checker

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """Prints a square with the character #.
+
+    Args:
+        size (int): size of the square.
+
+    Raises:
+        TypeError: if size is not an integer.
+        ValueError: if size is less than 0.
+
+    Returns:
+        None.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >=0")
+    if size == 0:
+        return
+    print(("#" * size + "\n") * size, end="")

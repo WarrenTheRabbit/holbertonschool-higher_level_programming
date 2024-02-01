61f7f89650251aafa6a96c1b6a454e844bc9f910
Warren <rabbitsinwarrens@gmail.com>
2024-02-01 22:21:30 +1100

    fix(task4): update error message

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -16,7 +16,7 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size must be >=0")
+        raise ValueError("size must be >= 0")
     if size == 0:
         return
     print(("#" * size + "\n") * size, end="")

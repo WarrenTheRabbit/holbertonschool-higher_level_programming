a3751c150f698782989474c88fdd57926e52ad8f
Warren <rabbitsinwarrens@gmail.com>
2024-02-05 23:42:25 +1100

    fix(task30): use function attribute

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    for i in range(10):
-        print(", ".join(["BestSchool"] * (i + 1)))
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.counter)

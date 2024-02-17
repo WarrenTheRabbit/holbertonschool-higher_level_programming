ce1ca84ab7850b69ce2e5b7c9221853c511055cc
Warren <rabbitsinwarrens@gmail.com>
2024-02-17 20:11:35 +1100

    feat(task2): submit to checker

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Module for is_same_class method."""
+
+def is_same_class(obj, a_class):
+    """Method to check if an object is an instance of a class."""
+    return type(obj) is a_class
+

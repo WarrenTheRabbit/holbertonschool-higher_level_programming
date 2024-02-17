38ba3aefeead7441c65c3a670618ae93e634e59b
Warren <rabbitsinwarrens@gmail.com>
2024-02-17 21:05:36 +1100

    feat(task6): submit to checker

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module for base_geometry class."""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+
+    def area(self):
+        """Method to compute the area of a geometry."""
+        raise Exception("area() is not implemented")

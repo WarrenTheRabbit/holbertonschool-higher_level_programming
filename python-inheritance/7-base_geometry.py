08792d01464914091a1628620ccf34b1a4e3ca12
Warren <rabbitsinwarrens@gmail.com>
2024-02-17 21:17:29 +1100

    feat(task7): submit to checker

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Module for base_geometry class."""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+
+    def area(self):
+        """Method to compute the area of a geometry."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method to validate the value."""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")

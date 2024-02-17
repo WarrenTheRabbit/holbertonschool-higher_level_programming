9e62fab1a5fc38174fd28662c64051e976355744
Warren <rabbitsinwarrens@gmail.com>
2024-02-17 21:56:34 +1100

    feat(task8): submit to checker

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""Module for base_geometry class."""
+
+
+class Rectangle(BaseGeometry):
+    """Class BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Method to initialize the rectangle."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Method to compute the area of a rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Method to compute the string representation of a rectangle."""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)

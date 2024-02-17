8d3c877a67fb21ffd90ebff8b04e69c3b384dc84
Warren <rabbitsinwarrens@gmail.com>
2024-02-17 20:32:30 +1100

    fix(task4): return False if actual type

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 
 def inherits_from(obj, a_class):
     """Method to check if an object is an instance of a class."""
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class

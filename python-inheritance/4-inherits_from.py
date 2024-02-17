68ce01b710ee6e54a922a70c35f55a75847672d7
Warren <rabbitsinwarrens@gmail.com>
2024-02-17 20:26:50 +1100

    feat(task4): submit to checker

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Module for inherits_from method."""
+
+
+def inherits_from(obj, a_class):
+    """Method to check if an object is an instance of a class."""
+    return isinstance(obj, a_class)

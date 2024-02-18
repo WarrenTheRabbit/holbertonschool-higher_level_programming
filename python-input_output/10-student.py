f3885662cfab8eb641d977c233d1b7211dc7199b
Warren <rabbitsinwarrens@gmail.com>
2024-02-18 23:32:01 +1100

    feat(task10): submit to checker

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module contains a basic student class."""
+
+
+class Student:
+    """Student class."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize student class."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the JSON representation of an object (string)"""
+        if attrs:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__

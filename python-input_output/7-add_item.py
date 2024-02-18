3afb79786f0314205a9ba85fa01a24c497869f35
Warren <rabbitsinwarrens@gmail.com>
2024-02-18 23:14:31 +1100

    fix(task7): create file if not exist

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 """Module contains a basic add item function."""
+from pathlib import Path
 import sys
-import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def add_item(*args):
+def add_item():
     """Add item to a list and save to a file."""
-    items = [arg for arg in sys.argv[1:]]
-    obj = load_from_json_file("add_item.json")
-    obj = obj + items
-    save_to_json_file(obj, "add_item.json")
+    filename = "add_item.json"
+    path = Path(filename)
+    print(path)
+    
+    if not path.is_file():
+        save_to_json_file([], filename)
+        
+    try: 
+        items = load_from_json_file(filename)
+    except Exception:
+        items = []
+        
+    items.extend(sys.argv[1:])
+    
+    save_to_json_file(items, filename)
+
+
+if __name__ == "__main__":
+    add_item()

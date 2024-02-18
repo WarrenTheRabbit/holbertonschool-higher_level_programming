7df4cf43cab05fcfef549c23fdb45ca0d8ba03c1
Warren <rabbitsinwarrens@gmail.com>
2024-02-18 22:50:46 +1100

    fix(task5): use with to write json object

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,4 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write JSON representation of an object to a file."""
-    json.dump(my_obj, filename)
+    with open(filename, "w", encoding='utf-8') as f:
+        return f.write(json.dumps(my_obj))

d9ba76d0ead8a6fcf128bd6ba4fa08d6d7420db6
Warren <rabbitsinwarrens@gmail.com>
2024-02-18 22:32:14 +1100

    fix(task1): return number of bytes written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,4 @@
 def write_file(filename="", text=""):
     """Wire file and print to stdout"""
     with open(filename, "w", encoding='utf-8') as f:
-        print(f.write(text))
+        return f.write(text)
